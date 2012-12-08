e1926ee48c22123b95c5e17aabc74e2087be2535
hei <hei@Nova-Box.(none)>
2012-12-08 14:31:01 +0800

    Added build script & folders

---
+++ build-all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# CM10 All Kernel Compiler
+# Date: 5/12/2012
+# By Hei1125
+
+# Build Hikari Kernel
+./build-hikari-kernel.sh
+
+# Build Nozomi Kernel
+clear && ./build-nozomi-kernel.sh
