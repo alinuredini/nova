dd232e08ff3c5aa6800264bb2483416ffa59528a
hei <hei@Nova-Box.(none)>
2012-12-10 20:27:30 +0800

    Enable 2D Overclocking

--- arch/arm/mach-msm/devices-msm8x60.c
+++ arch/arm/mach-msm/devices-msm8x60.c
@@ -1050,7 +1050,37 @@ static struct resource kgsl_2d0_resources[] = {
 		.flags = IORESOURCE_IRQ,
 	},
 };
-
+#ifdef CONFIG_GPU_OVERCLOCK
+static struct kgsl_device_platform_data kgsl_2d0_pdata = {
+	.pwrlevel = {
+		{
+			.gpu_freq = 266667000,
+			.bus_freq = 3,
+		},
+		{
+			.gpu_freq = 200000000,
+			.bus_freq = 2,
+		},
+		{
+			.gpu_freq = 96000000,
+			.bus_freq = 1,
+		},
+		{
+			.gpu_freq = 27000000,
+			.bus_freq = 0,
+		},
+	},
+	.init_level = 0,
+	.num_levels = 4,
+	.set_grp_async = NULL,
+	.idle_timeout = HZ/5,
+	.nap_allowed = true,
+	.clk_map = KGSL_CLK_CORE | KGSL_CLK_IFACE,
+#ifdef CONFIG_MSM_BUS_SCALING
+	.bus_scale_table = &grp2d0_bus_scale_pdata,
+#endif
+};
+#else
 static struct kgsl_device_platform_data kgsl_2d0_pdata = {
 	.pwrlevel = {
 		{
@@ -1080,6 +1110,7 @@ static struct kgsl_device_platform_data kgsl_2d0_pdata = {
 	.bus_scale_table = &grp2d0_bus_scale_pdata,
 #endif
 };
+#endif
 
 struct platform_device msm_kgsl_2d0 = {
 	.name = "kgsl-2d0",
@@ -1090,7 +1121,7 @@ struct platform_device msm_kgsl_2d0 = {
 		.platform_data = &kgsl_2d0_pdata,
 	},
 };
-
+#ifdef CONFIG_GPU_OVERCLOCK
 static struct resource kgsl_2d1_resources[] = {
 	{
 		.name = KGSL_2D1_REG_MEMORY,
@@ -1105,7 +1136,36 @@ static struct resource kgsl_2d1_resources[] = {
 		.flags = IORESOURCE_IRQ,
 	},
 };
-
+static struct kgsl_device_platform_data kgsl_2d1_pdata = {
+	.pwrlevel = {
+		{
+			.gpu_freq = 266667000,
+			.bus_freq = 3,
+		},
+		{
+			.gpu_freq = 200000000,
+			.bus_freq = 2,
+		},
+		{
+			.gpu_freq = 96000000,
+			.bus_freq = 1,
+		},
+		{
+			.gpu_freq = 27000000,
+			.bus_freq = 0,
+		},
+	},
+	.init_level = 0,
+	.num_levels = 4,
+	.set_grp_async = NULL,
+	.idle_timeout = HZ/5,
+	.nap_allowed = true,
+	.clk_map = KGSL_CLK_CORE | KGSL_CLK_IFACE,
+#ifdef CONFIG_MSM_BUS_SCALING
+	.bus_scale_table = &grp2d1_bus_scale_pdata,
+#endif
+};
+#else
 static struct kgsl_device_platform_data kgsl_2d1_pdata = {
 	.pwrlevel = {
 		{
@@ -1135,6 +1195,7 @@ static struct kgsl_device_platform_data kgsl_2d1_pdata = {
 	.bus_scale_table = &grp2d1_bus_scale_pdata,
 #endif
 };
+#endif
 
 struct platform_device msm_kgsl_2d1 = {
 	.name = "kgsl-2d1",
